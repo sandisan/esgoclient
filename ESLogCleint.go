4fe25e332d73bde2c8d6a3c8baf1dcc5319157ed
sandisan <sandicha@in.ibm.com>
2020-12-27 19:50:51 +0530

    Update ESLogCleint.go

--- ESLogCleint.go
+++ ESLogCleint.go
@@ -3,6 +3,8 @@ package main
 import (
 	"io"
 	"net/http"
+	"crypto/tls"
+	"io/ioutil"
 )
 
 func main() {
@@ -10,5 +12,22 @@ func main() {
 	http.ListenAndServe(":8080", nil)
 }
 func helloWorldHandler(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Hello world!")
+	
+    tr := &http.Transport{
+        TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+    }
+	
+    client := &http.Client{Transport: tr}
+    resp, err := client.Get("https://elasticsearch.ibm-common-services.svc:9200/_cluster/health")
+    if err != nil {
+	fmt.Println(err)
+	io.WriteString(w, err.Error())
+    }else {
+	    // read all response body
+	    data, _ := ioutil.ReadAll( resp.Body )
+	    // close response body
+	    resp.Body.Close()
+	    io.WriteString(w, string(resp))
+    }
+	
 }

54b6056a7f178e177f256b98e9c43f0d76952467
sandisan <sandicha@in.ibm.com>
2020-12-28 11:03:07 +0530

    Update ESLogCleint.go

--- ESLogCleint.go
+++ ESLogCleint.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"flag"
 	"log"
+	"bytes"
 	"net/http"
 	"crypto/tls"
 	"crypto/x509"
@@ -19,11 +20,13 @@ var (
 )
 
 func main() {
-	http.HandleFunc("/", helloWorldHandler)
+	http.HandleFunc("/getdata", getDataHandler)
+	http.HandleFunc("/postdata", postDataHandler)
+	http.HandleFunc("/putdata", putDataHandler)
 	http.ListenAndServe(":8080", nil)
 }
 
-func helloWorldHandler(w http.ResponseWriter, r *http.Request) {
+func getDataHandler(w http.ResponseWriter, r *http.Request) {
 
 	flag.Parse()
 
@@ -55,7 +58,135 @@ func helloWorldHandler(w http.ResponseWriter, r *http.Request) {
 	transport := &http.Transport{TLSClientConfig: tlsConfig}
 	client := &http.Client{Transport: transport}
 
-	resp, err := client.Get("https://elasticsearch:9200/_cluster/health")
+	resp, err := client.Get("https://elasticsearch:9200/ace-index/_search?pretty=true&q=*:*")
+	if err != nil {
+		fmt.Println(err)
+		io.WriteString(w, err.Error())
+		return
+	}else {
+	    // read all response body
+	    data, err := ioutil.ReadAll( resp.Body )
+	    if err != nil {
+		fmt.Println(err)
+		io.WriteString(w, err.Error())
+		return
+	    } else {
+		 // close response body
+		resp.Body.Close()
+		io.WriteString(w, string(data))  
+	    }
+	}
+	
+}
+
+func postDataHandler(w http.ResponseWriter, r *http.Request) {
+
+	// read the body
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(r.Body)
+
+	// get the payload and sink it
+	//payload := buf.Bytes()
+	
+	flag.Parse()
+
+	// Load client cert
+	cert, err := tls.LoadX509KeyPair(*certFile, *keyFile)
+	if err != nil {
+		log.Fatal(err)
+		io.WriteString(w, err.Error())
+		return
+	}
+
+	// Load CA cert
+	caCert, err := ioutil.ReadFile(*caFile)
+	if err != nil {
+		log.Fatal(err)
+		io.WriteString(w, err.Error())
+		return
+	}
+	caCertPool := x509.NewCertPool()
+	caCertPool.AppendCertsFromPEM(caCert)
+
+	// Setup HTTPS client
+	tlsConfig := &tls.Config{
+		Certificates: []tls.Certificate{cert},
+		RootCAs:      caCertPool,
+	}
+
+	tlsConfig.BuildNameToCertificate()
+	transport := &http.Transport{TLSClientConfig: tlsConfig}
+	client := &http.Client{Transport: transport}
+	
+	resp, err := http.Post("https://elasticsearch:9200/ace-index/_doc", "application/json", &buf)
+
+	//resp, err := client.Get("https://elasticsearch:9200/_cluster/health")
+	if err != nil {
+		fmt.Println(err)
+		io.WriteString(w, err.Error())
+		return
+	}else {
+	    // read all response body
+	    data, err := ioutil.ReadAll( resp.Body )
+	    if err != nil {
+		fmt.Println(err)
+		io.WriteString(w, err.Error())
+		return
+	    } else {
+		 // close response body
+		resp.Body.Close()
+		io.WriteString(w, string(data))  
+	    }
+	}
+	
+}
+
+func putDataHandler(w http.ResponseWriter, r *http.Request) {
+
+	// read the body
+	//buf := new(bytes.Buffer)
+	//buf.ReadFrom(r.Body)
+
+	// get the payload and sink it
+	//payload := buf.Bytes()
+	
+	flag.Parse()
+
+	// Load client cert
+	cert, err := tls.LoadX509KeyPair(*certFile, *keyFile)
+	if err != nil {
+		log.Fatal(err)
+		io.WriteString(w, err.Error())
+		return
+	}
+
+	// Load CA cert
+	caCert, err := ioutil.ReadFile(*caFile)
+	if err != nil {
+		log.Fatal(err)
+		io.WriteString(w, err.Error())
+		return
+	}
+	caCertPool := x509.NewCertPool()
+	caCertPool.AppendCertsFromPEM(caCert)
+
+	// Setup HTTPS client
+	tlsConfig := &tls.Config{
+		Certificates: []tls.Certificate{cert},
+		RootCAs:      caCertPool,
+	}
+
+	tlsConfig.BuildNameToCertificate()
+	transport := &http.Transport{TLSClientConfig: tlsConfig}
+	client := &http.Client{Transport: transport}
+	
+	request, err := http.NewRequest("PUT", "https://elasticsearch:9200/ace-index", nil)
+	request.ContentType = "application/json"
+	resp, err := client.Do(request)
+	
+	//resp, err := http.Post("https://elasticsearch:9200/_cluster/health", "application/json", &buf)
+	//resp, err := client.Get("https://elasticsearch:9200/_cluster/health")
+	
 	if err != nil {
 		fmt.Println(err)
 		io.WriteString(w, err.Error())

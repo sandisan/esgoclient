91be75b2fb511e42d6e5c23cb8ec4f6917a988f9
sandisan <sandicha@in.ibm.com>
2020-12-28 10:38:43 +0530

    Update ESLogCleint.go

--- ESLogCleint.go
+++ ESLogCleint.go
@@ -22,58 +22,9 @@ func main() {
 	http.HandleFunc("/", helloWorldHandler)
 	http.ListenAndServe(":8080", nil)
 }
+
 func helloWorldHandler(w http.ResponseWriter, r *http.Request) {
 
-// 	cert, err := ioutil.ReadFile("elk.crt")
-// 	if err != nil {
-// 		log.Fatalf("Couldn't load file", err)
-// 	}
-// 	certPool := x509.NewCertPool()
-// 	certPool.AppendCertsFromPEM(cert)
-
-// 	conf := &tls.Config{
-// 		RootCAs: certPool,
-// 		//InsecureSkipVerify: true,
-// 	}
-// //     tr := &http.Transport{
-// //         TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-// //     }
-//   tr := &http.Transport{
-//         TLSClientConfig: conf,
-//     }
-//      caCert, err := ioutil.ReadFile("elk-ca.pem")
-// 	caCert, err := ioutil.ReadFile("elk.crt")
-//       if err != nil {
-//               log.Fatal(err)
-// 	      io.WriteString(w, err.Error())
-// 	      return
-//        }
-//        caCertPool := x509.NewCertPool()
-//        caCertPool.AppendCertsFromPEM(caCert)
-// //       cert, err := tls.LoadX509KeyPair("elk-crt.pem", "elk-key.pem")
-// //        if err != nil {
-// //                log.Fatal(err)
-// // 	       io.WriteString(w, err.Error())
-// // 	      return
-// //        }
-
-// //         client := &http.Client{
-// //                 Transport: &http.Transport{
-// //                         TLSClientConfig: &tls.Config{
-// //                                 RootCAs:      caCertPool,
-// //                                Certificates: []tls.Certificate{cert},
-// //                         },
-// //                 },
-// //         }
-//     //client := &http.Client{Transport: tr}
-// 	client := &http.Client{
-//         Transport: &http.Transport{
-//             TLSClientConfig: &tls.Config{
-//                 RootCAs:      caCertPool,
-//             },
-//         },
-//     }
-	
 	flag.Parse()
 
 	// Load client cert
@@ -99,17 +50,17 @@ func helloWorldHandler(w http.ResponseWriter, r *http.Request) {
 		Certificates: []tls.Certificate{cert},
 		RootCAs:      caCertPool,
 	}
-	
+
 	tlsConfig.BuildNameToCertificate()
 	transport := &http.Transport{TLSClientConfig: tlsConfig}
 	client := &http.Client{Transport: transport}
-	
-    resp, err := client.Get("https://elasticsearch:9200/_cluster/health")
-    if err != nil {
-	fmt.Println(err)
-	io.WriteString(w, err.Error())
-	return
-    }else {
+
+	resp, err := client.Get("https://elasticsearch:9200/_cluster/health")
+	if err != nil {
+		fmt.Println(err)
+		io.WriteString(w, err.Error())
+		return
+	}else {
 	    // read all response body
 	    data, err := ioutil.ReadAll( resp.Body )
 	    if err != nil {
@@ -117,10 +68,10 @@ func helloWorldHandler(w http.ResponseWriter, r *http.Request) {
 		io.WriteString(w, err.Error())
 		return
 	    } else {
-	  	 // close response body
-	    	resp.Body.Close()
-	    	io.WriteString(w, string(data))  
+		 // close response body
+		resp.Body.Close()
+		io.WriteString(w, string(data))  
 	    }
-    }
+	}
 	
 }
